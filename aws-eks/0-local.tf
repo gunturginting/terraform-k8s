29be874f767e900613f1bf4a6c3caa7899f44818
Guntur Ginting <guntur.putra@aleph-labs.com>
2024-08-06 17:37:37 +0700

    first commit

--- aws-eks/0-local.tf
+++ aws-eks/0-local.tf
@@ -4,5 +4,5 @@ locals {
     zone-1      = "ap-southeast-1a"
     zone-2      = "ap-southeast-1b"
     zone-3      = "ap-southeast-1c"
-    eks_name    = "cluster-eks"
+    eks_name    = "ecosystem"
 }
---
+++ aws-eks/7-eksMaster.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "iam_role_eks" {
+    assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_eks" {
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role = aws_iam_role.iam_role_eks.name
+}
+
+resource "aws_eks_cluster" "eks" {
+    name = "${local.eks_name}-eks-cluster"
+    role_arn = aws_iam_role.iam_role_eks.arn
+
+    vpc_config {
+      subnet_ids = [
+        aws_subnet.private_subnet_1.id,
+        aws_subnet.private_subnet_2
+      ]
+
+      endpoint_private_access = true
+      endpoint_public_access = false
+    }
+
+    access_config {
+      authentication_mode = "API"
+      bootstrap_cluster_creator_admin_permissions = true
+    }
+
+    depends_on = [ aws_iam_role_policy_attachment.role_policy_eks ]
+}
