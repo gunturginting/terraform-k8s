cbdbdaed95762863ce6cf9d09e93eed2171a2fee
Guntur Ginting <guntur.putra@aleph-labs.com>
2024-08-12 14:42:15 +0700

    add adjustment plan

--- aws-eks/9-iamDeveloperUser.tf
+++ aws-eks/9-iamDeveloperUser.tf
@@ -1,16 +1,8 @@
-variable "create_developer_user" {
-  description = "Set this to true to create the developer user"
-  type        = bool
-  default     = false
-}
-
 resource "aws_iam_user" "developer" {
-    count = var.create_developer_user ? 1 : 0
     name = "developer"
 }
 
 resource "aws_iam_policy" "developer_eks" {
-    count = var.create_developer_user ? 1 : 0
     name = "AmazonEKSDeveloperPolicy"
 
     policy = <<POLICY
@@ -31,13 +23,11 @@ POLICY
 }
 
 resource "aws_iam_user_policy_attachment" "developer_eks" {
-    count = var.create_developer_user ? 1 : 0
     user = aws_iam_user.developer.name
     policy_arn = aws_iam_policy.developer_eks.arn
 }
 
 resource "aws_eks_access_entry" "developer" {
-    count = var.create_developer_user ? 1 : 0
     cluster_name =   aws_eks_cluster.eks.name
     principal_arn = aws_iam_user.developer.arn
     kubernetes_groups = [ "my-viewer" ]
--- aws-eks/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-create_developer_user = true
-create_admin_user     = false
